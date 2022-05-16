fd0cd6ccb3b4a0cd479c8fd32e23db5679f18ae3
Alexis Melia <39966501+Alexismla@users.noreply.github.com>
2022-05-16 10:04:07 +0200

    création du dossier models && fichier Bddconnect && La connexion à mongoDB

---
+++ models/bddconnect.js
@@ -0,0 +1,13 @@
+var mongoose = require("mongoose");
+
+var options = {
+  connectTimeoutMS: 5000,
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+};
+
+mongoose.connect("", options, function (err) {
+  console.log(err);
+});
+
+module.exports = mongoose;
