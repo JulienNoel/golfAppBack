5773cbae76f96c118211ae0faf49828bc1281e02
Edouard Roussel <edouardroussel@Edouards-MacBook-Pro.local>
2022-05-26 09:12:08 +0200

    jeudi

--- routes/index.js
+++ routes/index.js
@@ -76,198 +76,196 @@ router.get("/askgolf", async function (req, res, next) {
 });
 
 //Route pour créer notre Collection Golf
-router.post("/golfAdd", async function (req, res, next) {
-  var golf = [];
-  var par = [4, 4, 3, 4, 4, 3, 4, 5, 5, 4, 3, 5, 4, 4, 4, 4, 3, 4];
-  var distance = [
-    389, 284, 142, 297, 357, 160, 319, 472, 461, 319, 153, 397, 370, 320, 328,
-    379, 169, 360,
-  ];
+// router.post("/golfAdd", async function (req, res, next) {
+//   var golf = [];
+//   var par = [4, 4, 3, 4, 4, 3, 4, 5, 5, 4, 3, 5, 4, 4, 4, 4, 3, 4];
+//   var distance = [
+//     389, 284, 142, 297, 357, 160, 319, 472, 461, 319, 153, 397, 370, 320, 328,
+//     379, 169, 360,
+//   ];
   
 
 
-  function randomGolf(index, longueurTrou) {
-    var tableauScore = []
-    var parcours1 = { nomParcours: nomParcours[index] }
-    var tableauImage = [
-      'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399509/golf/vineuil-1-sm_x7dn5o.jpg',
-      'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399515/golf/vineuil-2-sm_wr4gxs.jpg',
-      'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399518/golf/vineuil-3-sm_oghwtx.jpg',
-      'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399521/golf/vineuil-4-sm_gfymyg.jpg',
-      'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399524/golf/vineuil-5-sm_lchlaz.jpg',
-      'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399530/golf/vineuil-6-sm_pxw000.jpg',
-      'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399527/golf/vineuil-7-sm_z4fsly.jpg',
-      'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399533/golf/vineuil-8-sm_j4seyc.jpg',
-      'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399536/golf/vineuil-9-sm_eniu6e.jpg',
-      'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399539/golf/vineuil-10-sm_yxg9zv.jpg',
-      'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399542/golf/vineuil-11-sm_fqpopc.jpg',
-      'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399491/golf/vineuil-12-sm_v81obm.jpg',
-      'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399494/golf/vineuil-13-sm_cnd9z5.jpg',
-      'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399497/golf/vineuil-14-sm_xm0ait.jpg',
-      'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399500/golf/vineuil-15-sm_tjen44.jpg',
-      'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399503/golf/vineuil-16-sm.jpg',
-      'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399506/golf/vineuil-17-sm.jpg',
-      'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399512/golf/vineuil-18-sm.jpg',
-      ]
-  var nomParcours = [
-    "Parcours belle vue",
-    "Parcours des flots bleus",
-    "Parcours des érables",
-    "Parcours beau soleil",
-    "Parcours des amoureux",
-    "Parcours d'Alexis 1er",
-    "Parcours Shaddy",
-    "Parcours de Paris",
-    "Parcours de la bonne fortune",
-    "Parcours du ruisseau",
-    "Parcours de la cascade",
-    "Parcours des rosseaux",
-    "Parcours de la tranquilité",
-    "Parcours des hérables",
-    "Parcours du débutant",
-    "Parcours Wood",
-    "Parcours de la vallée",
-    "Parcours de la dune Bleu",
-    "Parcours de la Capsule",
-    "Parcours hole in one",
-  ];
+//   function randomGolf(index, longueurTrou) {
+//     var tableauScore = []
+//     var parcours1 = { nomParcours: nomParcours[index] }
+//     var tableauImage = [
+//     'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399509/golf/vineuil-1-sm_x7dn5o.jpg',
+//     'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399515/golf/vineuil-2-sm_wr4gxs.jpg',
+//     'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399518/golf/vineuil-3-sm_oghwtx.jpg',
+//     'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399521/golf/vineuil-4-sm_gfymyg.jpg',
+//     'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399524/golf/vineuil-5-sm_lchlaz.jpg',
+//     'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399530/golf/vineuil-6-sm_pxw000.jpg',
+//     'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399527/golf/vineuil-7-sm_z4fsly.jpg',
+//     'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399533/golf/vineuil-8-sm_j4seyc.jpg',
+//     'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399536/golf/vineuil-9-sm_eniu6e.jpg',
+//     'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399539/golf/vineuil-10-sm_yxg9zv.jpg',
+//     'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399542/golf/vineuil-11-sm_fqpopc.jpg',
+//     'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399491/golf/vineuil-12-sm_v81obm.jpg',
+//     'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399494/golf/vineuil-13-sm_cnd9z5.jpg',
+//     'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399497/golf/vineuil-14-sm_xm0ait.jpg',
+//     'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399500/golf/vineuil-15-sm_tjen44.jpg',
+//     'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399503/golf/vineuil-16-sm.jpg',
+//     'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399506/golf/vineuil-17-sm.jpg',
+//     'https://res.cloudinary.com/dqvhyz0rs/image/upload/v1653399512/golf/vineuil-18-sm.jpg',
+//     ]
+//   var nomParcours = [
+//     "Parcours belle vue",
+//     "Parcours des flots bleus",
+//     "Parcours des érables",
+//     "Parcours beau soleil",
+//     "Parcours des amoureux",
+//     "Parcours d'Alexis 1er",
+//     "Parcours Shaddy",
+//     "Parcours de Paris",
+//     "Parcours de la bonne fortune",
+//     "Parcours du ruisseau",
+//     "Parcours de la cascade",
+//     "Parcours des rosseaux",
+//     "Parcours de la tranquilité",
+//     "Parcours des hérables",
+//     "Parcours du débutant",
+//     "Parcours Wood",
+//     "Parcours de la vallée",
+//     "Parcours de la dune Bleu",
+//     "Parcours de la Capsule",
+//     "Parcours hole in one",
+//   ];}
 
-  function randomGolf(index, longueurTrou) {
-    var tableauScore = [];
-    var parcours1 = { nomParcours: nomParcours[index] };
+//   function randomGolf(index, longueurTrou) {
+//     var tableauScore = [];
+//     var parcours1 = { nomParcours: nomParcours[index] };
 
-    for (var i = 1; i <= longueurTrou; i++) {
-      var parcoursTrou = {};
-      parcoursTrou.trou = i;
-      parcoursTrou.par = par[i - 1];
-      parcoursTrou.url = tableauImage[i - 1]
-      parcoursTrou.distance = distance[i - 1];
-      tableauScore.push(parcoursTrou)
-    }
-    parcours1.parcoursTrou = tableauScore
-    return parcours1
-  }
+//     for (var i = 1; i <= longueurTrou; i++) {
+//       var parcoursTrou = {};
+//       parcoursTrou.trou = i;
+//       parcoursTrou.par = par[i - 1];
+//       parcoursTrou.url = tableauImage[i - 1]
+//       parcoursTrou.distance = distance[i - 1];
+//       tableauScore.push(parcoursTrou)
+//     }
+//     parcours1.parcoursTrou = tableauScore
+//     return parcours1
+//   }
 
 
-  for (var i = 0; i < 11; i++) {
-    var randomBool1 = Math.random() > 0.5 ? true : false;
-    var randomBool2 = Math.random() > 0.5 ? true : false;
+//   // for (var i = 0; i < 11; i++) {
+//   //   var randomBool1 = Math.random() > 0.5 ? true : false;
+//   //   var randomBool2 = Math.random() > 0.5 ? true : false;
 
-    golf.push({
-      golfName: `golf ${i}`,
-      practice: randomBool1,
-      restauration: randomBool2,
-      dixhuitTrous: 1,
-      neufTrous: 1,
-      golfAddress: {
-        golfCity: "Paris",
-        golfPostCode: "75017",
-<<<<<<< HEAD
-<<<<<<< HEAD
-        golfAddressName: `5${i} boulevard Peirrere`,
-        golfLatitude: parseFloat(48.875 + i / 10),
-        golfLongitude: parseFloat(2.33 + i / 10),
-      },
+//   //   golf.push({
+//   //     golfName: `golf ${i}`,
+//   //     practice: randomBool1,
+//   //     restauration: randomBool2,
+//   //     dixhuitTrous: 1,
+//   //     neufTrous: 1,
+//   //     golfAddress: {
+//   //       golfCity: "Paris",
+//   //       golfPostCode: "75017",
 
-      parcours: [randomGolf(0, 9), randomGolf(1, 18)],
-=======
-        golfAddressName: `5${i} boulevard Pereire`,
-        golfLatitude: parseFloat(48.875 +  i/10 ),
-        golfLongitude: parseFloat(2.33 +  i/10 ),
-      },
+//   //       golfAddressName: `5${i} boulevard Peirrere`,
+//   //       golfLatitude: parseFloat(48.875 + i / 10),
+//   //       golfLongitude: parseFloat(2.33 + i / 10),
+//   //     },
 
-      parcours: [randomGolf(0,9),randomGolf(1,18)]
->>>>>>> 97b82ce0f6797be8877a12f84dcb68e066dda47b
-=======
-        golfAddressName: `5${i} boulevard Peirrere`,
-        golfLatitude: parseFloat(48.875 + i / 10),
-        golfLongitude: parseFloat(2.33 + i / 10),
-      },
+//   //     parcours: [randomGolf(0, 9), randomGolf(1, 18)],
 
-      parcours: [randomGolf(0, 9), randomGolf(1, 18)],
->>>>>>> 7c6aa683c158e90fd35148f71c8ab15726a0db67
-    });
+//   //       golfAddressName: `5${i} boulevard Pereire`,
+//   //       golfLatitude: parseFloat(48.875 +  i/10 ),
+//   //       golfLongitude: parseFloat(2.33 +  i/10 ),
+//   //     },
 
-    var newGolf = new GolfModel({
-      golfName: golf[i].golfName,
-      practice: golf[i].practice,
-      restauration: golf[i].restauration,
-      dixhuitTrous: golf[i].dixhuitTrous,
-      neufTrous: golf[i].neufTrous,
-      golfCity: golf[i].golfCity,
-      golfAddress: golf[i].golfAddress,
-      golfPostCode: golf[i].golfPostCode,
-      parcours: golf[i].parcours,
-    });
-    var golfSaved = await newGolf.save();
-  }
-  res.json(golfSaved);
-});
+//   //     parcours: [randomGolf(0,9),randomGolf(1,18)]
 
-router.post("/register", async function (req, res, next) {
-  var result = false;
-  var user = null;
-  var error = [];
+//   //       golfAddressName: `5${i} boulevard Peirrere`,
+//   //       golfLatitude: parseFloat(48.875 + i / 10),
+//   //       golfLongitude: parseFloat(2.33 + i / 10),
+//   //     },
 
-  const data = await userModel.findOne({
-    mail: req.body.emailFromFront,
-  });
+//   //     parcours: [randomGolf(0, 9), randomGolf(1, 18)],
 
-  if (data != null) {
-    error.push("utilisateur déjà présent");
-  }
+//   //   });
 
-  if (
-    req.body.passwordFromFront == "" ||
-    req.body.userNameFromFront == "" ||
-    req.body.prenomFromFront == "" ||
-    req.body.birthDateFromFront == ""
-  ) {
-    error.push("Des champs sont vides");
-  }
+//     var newGolf = new GolfModel({
+//       golfName: golf[i].golfName,
+//       practice: golf[i].practice,
+//       restauration: golf[i].restauration,
+//       dixhuitTrous: golf[i].dixhuitTrous,
+//       neufTrous: golf[i].neufTrous,
+//       golfCity: golf[i].golfCity,
+//       golfAddress: golf[i].golfAddress,
+//       golfPostCode: golf[i].golfPostCode,
+//       parcours: golf[i].parcours,
+//     });
+//     var golfSaved = await newGolf.save();
+//   }
+//   res.json(golfSaved);
+// });
 
-  // var regexMail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+// router.post("/register", async function (req, res, next) {
+//   var result = false;
+//   var user = null;
+//   var error = [];
 
-  // if (!regexMail.test(req.body.emailFromFront)) {
-  //   error.push("Email Incorrect");
-  // }
+//   const data = await userModel.findOne({
+//     mail: req.body.emailFromFront,
+//   });
 
-  // var regexPassword = /^(?=.{8,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$/;
+//   if (data != null) {
+//     error.push("utilisateur déjà présent");
+//   }
 
-  // if (!regexPassword.test(req.body.passwordFromFront)) {
-  //   error.push(
-  //     "Mot de Passe Incorrect doit contenir au moins 8 charactères, 1 majuscule, 1 minuscule et 1 chiffre"
-  //   );
-  // }
-  // var regexBirthDate = /^[0-3]?[0-9].[0-3]?[0-9].(?:[0-9]{2})?[0-9]{2}$/;
-  // if (
-  //   !regexBirthDate.test(req.body.birthDateFromFront) &&
-  //   req.body.birthDateFromFront.length < 10
-  // ) {
-  //   error.push("Date de naissance incorrect");
-  // }
+//   if (
+//     req.body.passwordFromFront == "" ||
+//     req.body.userNameFromFront == "" ||
+//     req.body.prenomFromFront == "" ||
+//     req.body.birthDateFromFront == ""
+//   ) {
+//     error.push("Des champs sont vides");
+//   }
 
-  if (error.length == 0) {
-    var hash = bcrypt.hashSync(req.body.passwordFromFront, 10);
+//   // var regexMail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 
-    var newUser = new userModel({
-      mail: req.body.emailFromFront,
-      password: hash,
-      token: uid2(32),
-      userName: req.body.userNameFromFront,
-      userPrenom: req.body.prenomFromFront,
-      birthDate: req.body.birthDateFromFront,
-    });
+//   // if (!regexMail.test(req.body.emailFromFront)) {
+//   //   error.push("Email Incorrect");
+//   // }
 
-    var user = await newUser.save();
+//   // var regexPassword = /^(?=.{8,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$/;
 
-    if (user) {
-      result = true;
-    }
-  }
+//   // if (!regexPassword.test(req.body.passwordFromFront)) {
+//   //   error.push(
+//   //     "Mot de Passe Incorrect doit contenir au moins 8 charactères, 1 majuscule, 1 minuscule et 1 chiffre"
+//   //   );
+//   // }
+//   // var regexBirthDate = /^[0-3]?[0-9].[0-3]?[0-9].(?:[0-9]{2})?[0-9]{2}$/;
+//   // if (
+//   //   !regexBirthDate.test(req.body.birthDateFromFront) &&
+//   //   req.body.birthDateFromFront.length < 10
+//   // ) {
+//   //   error.push("Date de naissance incorrect");
+//   // }
 
-  res.json({ result, error, user });
-});
+//   if (error.length == 0) {
+//     var hash = bcrypt.hashSync(req.body.passwordFromFront, 10);
+
+//     var newUser = new userModel({
+//       mail: req.body.emailFromFront,
+//       password: hash,
+//       token: uid2(32),
+//       userName: req.body.userNameFromFront,
+//       userPrenom: req.body.prenomFromFront,
+//       birthDate: req.body.birthDateFromFront,
+//     });
+
+//     var user = await newUser.save();
+
+//     if (user) {
+//       result = true;
+//     }
+//   }
+
+//   res.json({ result, error, user });
+// });
 
 router.post("/login", async function (req, res, next) {
   var result = false;
